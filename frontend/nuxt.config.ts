230904eb3efc6b76ca8b7cc81e10a7fecd97b5eb
Sébastien Denooz <sebastien.denooz@gmail.com>
2025-08-29 13:00:17 +0200

    #4 : Create simple local config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -16,7 +16,24 @@ export default defineNuxtConfig({
     '@nuxtjs/i18n'
   ],
 
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        file: 'en.json'
+      }
+    ],
+    defaultLocale: 'en',
+    langDir: 'locales/',
+    strategy: 'no_prefix',
+    vueI18n: 'i18n.config.ts'
+  },
+
   sanctum: {
       baseUrl: 'http://localhost:8000', // Laravel API
+  },
+  appConfig: {
+    apiUrl: process.env.API_URL,
   },
 })
